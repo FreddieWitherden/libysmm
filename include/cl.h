14f8739304eabcd7b11f21437e574943bc205b40
Freddie Witherden <freddie@witherden.org>
2022-06-24 14:22:27 -0500

    Fix kernel execution.

--- include/cl.h
+++ include/cl.h
@@ -60,6 +60,9 @@ libysmm_cl_destory_smm_kernel(
 cl_int
 libysmm_cl_enqueue_smm_kernel(
     libysmm_cl_smm_kernel_t smmk,
+    cl_mem a,
+    cl_mem b,
+    cl_mem c,
     cl_command_queue queue,
     cl_uint num_events_in_wait_list,
     const cl_event* event_wait_list,
